ab69a5ea315044067e31e3ff706418d3d7171607
jinman190 <jinman190@gmail.com>
2017-09-30 11:14:25 -0400

    첫 cnn에 레이어 늘리기 완성
    레이어 늘리고 드롭아웃 레이어도 추가

--- EveryoneDL/EveryoneDL11_4.py
+++ EveryoneDL/EveryoneDL11_4.py
@@ -9,6 +9,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+keep_prob = tf.placeholder(tf.float32)
 
 X = tf.placeholder(tf.float32, [None, 28*28])
 X_img = tf.reshape(X, [-1, 28, 28, 1])
@@ -41,13 +42,13 @@ learning_rate=0.01
 #dropout 레이어 하나 추가
 W4 = tf.get_variable("W4", shape=[4*4*128, 625], initializer=tf.contrib.layers.xavier_initializer())
 b4 = tf.Variable(tf.random_normal([625]))
-L4 = tf.nn.relu(ft.matmul(L3, W4) + b4)
-L4 = tf.nn.dropout(L4, keep_prob=0.7) #dropout 확률
+L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob) #dropout 확률
 
 
-W5 = tf.get_variable("W4", shape=[4*4*128, 10], initializer=tf.contrib.layers.xavier_initializer())
+W5 = tf.get_variable("W5", shape=[625, 10], initializer=tf.contrib.layers.xavier_initializer())
 b = tf.Variable(tf.random_normal([10]))
-hypothesis = tf.matmul(L3, W4) + b
+hypothesis = tf.matmul(L4, W5) + b
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
@@ -63,7 +64,7 @@ for epoch in range(training_epochs):
     total_batch = int(mnist.train.num_examples / batch_size)
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        c, _ = sess.run([cost, optimizer], feed_dict={X:batch_xs, Y:batch_ys})
+        c, _ = sess.run([cost, optimizer], feed_dict={X:batch_xs, Y:batch_ys, keep_prob:0.7}) #학습은 drop out 70%로
         avg_cost += c / total_batch
 
     print('Epoch: ', (epoch+1), ' cost: ', avg_cost)
@@ -71,4 +72,4 @@ for epoch in range(training_epochs):
 
 prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
-print('acurracy: ', sess.run(accuracy, feed_dict={X:mnist.test.images, Y:mnist.test.labels}))
+print('acurracy: ', sess.run(accuracy, feed_dict={X:mnist.test.images, Y:mnist.test.labels, keep_prob:1})) #테스트는 drop out 없이
